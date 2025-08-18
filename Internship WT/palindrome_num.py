3c9aefb3847507c6e5d5fde27f8673f45c32bf30
Gayatri Bajirao Jadhav <117456242+gbj20@users.noreply.github.com>
2025-08-18 18:02:58 +0530

    Update palindrome_num.py

--- Internship WT/palindrome_num.py
+++ Internship WT/palindrome_num.py
@@ -1,9 +1,22 @@
+# n = 121
+# n = str(n)
+
+# reverse = n[::-1]
+
+# if (n == reverse):
+#     print("palindrome")
+# else:
+#     print("Not Palindrome")
+################################################
 n = 121
-n = str(n)
+a = n//100 #return 1
+b = n % 100 # return 21
+c = b//10 # return 2
+d = b % 10 # return 1
 
-reverse = n[::-1]
-
-if (n == reverse):
+if (a == d):
     print("palindrome")
 else:
     print("Not Palindrome")
+
+
