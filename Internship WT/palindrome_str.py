8f35dd54065cf848d678d1cf142a686b5a5daa52
Gayatri Bajirao Jadhav <117456242+gbj20@users.noreply.github.com>
2025-08-18 18:17:44 +0530

    Update palindrome_str.py

--- Internship WT/palindrome_str.py
+++ Internship WT/palindrome_str.py
@@ -1,6 +1,20 @@
+# str = "madam"
+# reverse = str[::-1]
+# if (str == reverse):
+#     print("palindrome")
+# else:
+#     print("Not Palindrome")
+
+#########################
+# using for Loop
+
 str = "madam"
 reverse = str[::-1]
-if (str == reverse):
-    print("palindrome")
-else:
-    print("Not Palindrome")
+
+for char in str:  # reverse = char + reverse
+    if str == reverse:
+        print("palindrome")
+    else:
+        print("Not palindrome")
+    break
+
